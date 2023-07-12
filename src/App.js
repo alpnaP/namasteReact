3b426096139e4d7fdff5c4897d937fc2598f89f2
Patel <alpna.patel@capgemini.com>
2023-07-12 12:36:48 +0530

    recent ep-02

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/Header";
-import Body from "./components/Body";
 
 const AppLayout = () => {
-  return (
-    <div className="app">
-      <Header />
-      <Body />
-    </div>
-  );
+  return <h1>hi aldjioa</h1>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
