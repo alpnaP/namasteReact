239e8fe97056a9b5f29a00103341a380f0e4341c
Patel <alpna.patel@capgemini.com>
2023-07-12 13:31:39 +0530

    Assignment-01 and EP-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { createElement } from "react";
 import ReactDOM from "react-dom/client";
 
-const AppLayout = () => {
-  return <h1>hi aldjioa</h1>;
-};
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "namaste react :)"
+// );
+// console.log(heading);
+
+const heading = (
+  <h1 className="heading" tabIndex={1}>
+    hi from jsx
+  </h1>
+);
+console.log(heading);
+
+//React component
+//class based component
+//Functional component
+// const AppLayout = () => {
+//   return (
+//     <div className="app">
+//       <h1>Hi Alpnaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>
+//     </div>
+//   );
+// };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
 
-root.render(<AppLayout />);
+// root.render(<AppLayout />);
